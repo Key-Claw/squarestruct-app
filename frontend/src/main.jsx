74f1a0369ded7287a80030a680a450b7ab5da9b6
Ragames1313 <rzarzuelor@gmail.com>
2026-04-27 20:17:26 +0200

    Mejorada la barra de navegación, optimizada para la version movil y añadido el logotipo

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 // import 'bootstrap/dist/js/bootstrap.bundle.min.js' // Si usamos modales o componentes que requieren JS de Bootstrap
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
