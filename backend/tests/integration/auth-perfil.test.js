7d301eff63a2b544d00292e2b45c4e38b7acd018
chipsentinel <chipsentinel@gmail.com>
2026-05-21 21:37:32 +0200

    test(backend): importar db en pruebas de autenticacion

--- backend/tests/integration/auth-perfil.test.js
+++ backend/tests/integration/auth-perfil.test.js
@@ -1,5 +1,5 @@
 import request from 'supertest';
-import app from '../../src/app.js';
+import app, { db } from '../../src/app.js';
 
 describe('Registro y login de usuario', () => {
   const email = `test${Date.now()}@mail.com`;
